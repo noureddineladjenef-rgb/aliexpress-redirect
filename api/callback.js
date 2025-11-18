b9fc5d96796182516b1f0d263b19a3e91cb33bfd
noureddineladjenef-rgb <noureddineladjenef@gmail.com>
2025-11-18 12:23:34 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,15 +1,4 @@
 export default function handler(req, res) {
   const { code } = req.query;
 
-  // التحقق إذا كان الكود غير موجود
-  if (!code) {
-    return res.status(400).json({ error: "Missing code" });
-  }
-
-  // إذا استلم الكود بنجاح
-  return res.status(200).json({
-    success: true,
-    message: "Callback received successfully",
-    code: code
-  });
-}
+  if (!
