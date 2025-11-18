7c7c9c06884afeb4151b81ac2c76cb16c733a6eb
noureddineladjenef-rgb <noureddineladjenef@gmail.com>
2025-11-18 12:29:23 +0100

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,11 +1,4 @@
 export default function handler(req, res) {
-  const redirectUri = process.env.REDIRECT_URI;
-
-  if (!redirectUri) {
-    return res.status(500).json({ error: "Missing REDIRECT_URI in environment variables" });
-  }
-
-  const authUrl = `https://auth.aliexpress.com/oauth/authorize?response_type=code&client_id=${process.env.APP_KEY}&redirect_uri=${encodeURIComponent(redirectUri)}&state=12345`;
-
+  const authUrl = `https://login.aliexpress.com/oauth2/authorize?response_type=code&force_auth=true&client_id=${process.env.APP_KEY}&redirect_uri=${process.env.REDIRECT_URI}`;
   res.redirect(authUrl);
 }
