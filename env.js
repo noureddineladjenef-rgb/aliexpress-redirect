a5153f8792e07ee576e783ccd84e6c25354b7d5a
noureddineladjenef-rgb <noureddineladjenef@gmail.com>
2025-11-20 01:05:07 +0100

    Create env.js

---
+++ env.js
@@ -0,0 +1,15 @@
+// api/env.js
+
+export default {
+  ALIEXPRESS_APP_KEY: "503368",
+
+  ALIEXPRESS_APP_SECRET: "elN0hkFZhXbUKORbj0L35409sRnPZ1eq",
+
+  ALIEXPRESS_AUTH_CODE: "3_503366_HVLPOMKPukktQv06ctvsqqIk1687",
+
+  ALIEXPRESS_REDIRECT_URI: "https://aliexpress-redirect.vercel.app/api/callback",
+
+  TELEGRAM_BOT_TOKEN: "8548245901:AAFbF4m_op83bfP8RoFdN0nyaiboReFge8Y",
+
+  TELEGRAM_CHAT_ID: "5267862493"
+};
